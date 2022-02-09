58df5b83ea39f6fafb6bcd9773fc25bf34e8e9af
AHpx <AHpx@yandex.com>
2022-02-09 14:33:08 +0800

    Add project files.

---
+++ AHpx.RG/App.axaml.cs
@@ -0,0 +1,29 @@
+using AHpx.RG.ViewModels;
+using AHpx.RG.Views;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace AHpx.RG
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ AHpx.RG/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHpx.RG.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
